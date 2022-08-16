484734bacaf90156a854985ae4a9f04e10224afa
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2022-08-16 20:28:19 -0300

    Implementações aula

---
+++ Aula.Tipos.Inteiros/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula.Tipos.Inteiros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 1;
+            int numero2 = 2;
+
+            numero2 = numero1;
+            numero1 = 10;
+
+            Console.WriteLine("Numero 1: " + numero1);
+            Console.WriteLine("Numero 2: " + numero2);
+            Console.WriteLine("Soma: " + (numero1 + numero2));
+            Console.WriteLine("Subtração: " + (numero1 - numero2));
+            Console.WriteLine("Multiplicação: " + (numero1 * numero2));
+            Console.WriteLine("Divisão: " + (numero1 / numero2));
+            Console.WriteLine("Divisão: " + (numero1 % numero2));
+        }
+    }
+}
